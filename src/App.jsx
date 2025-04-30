b33b94e6cf368bbd8cce10fba0094d9f3ac51bf9
Connor Gunter <bigc5674@gmail.com>
2025-04-30 13:20:16 -0400

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from "./components/Header/header";
+function App() {
+  return (
+    <>
+      <div>
+        NEF Landscaping
+        <Header />
+        </div>
+    </>
+  )
+}
+
+export default App

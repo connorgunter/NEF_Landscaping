a0d92fecc3fb088fc05f35ba483b1bb2d1df0585
Connor Gunter <bigc5674@gmail.com>
2025-05-11 13:01:27 -0400

    netlify form setup

--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -5,7 +5,7 @@ const Form = () => {
     return (
         <div className="form-container">
             <h1>Contact Us</h1>
-            <form name="contact" netlify>
+            <form name="contact" method="POST" data-netlify="true">
                 <input type="hidden" name="form-name" value="contact" />
                 <input placeholder="Name" type="text" id="name" name="name" required />
                 <input placeholder="E-Mail" type="email" id="email" name="email" required />

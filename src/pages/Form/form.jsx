c4a1dc21c493e3e0bf64ae112c41f74f497b2c11
Connor Gunter <bigc5674@gmail.com>
2025-05-11 12:57:39 -0400

    netlify form

--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -6,7 +6,7 @@ const Form = () => {
         <div className="form-container">
             <h1>Contact Us</h1>
             <form name="contact" netlify>
-                
+                <input type="hidden" name="form-name" value="contact" />
                 <input placeholder="Name" type="text" id="name" name="name" required />
                 <input placeholder="E-Mail" type="email" id="email" name="email" required />
                 <input placeholder="Phone Number" type="phonenumber" id="phonenumber" name="phonenumber" required />

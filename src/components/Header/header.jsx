e21e27c8899b4cc1d5d97a76fdcc9a152fdbeeb6
Connor Gunter <bigc5674@gmail.com>
2025-05-17 12:56:50 -0400

    mobile responsiveness, updated header section

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -15,22 +15,23 @@ const Header = () => {
 
   return (
     <header className="header-container" data-aos="fade-down">
-      <div className="tree-logo-container">
-        <img src={treeLogo} alt="Tree Logo" className="tree-logo" />
-        <div className="falling-leaf leaf1"><img src={leaf1} alt="Leaf 1" /></div>
-        <div className="falling-leaf leaf2"><img src={leaf2} alt="Leaf 2" /></div>
-        <div className="falling-leaf leaf3"><img src={leaf3} alt="Leaf 3" /></div>
+      <div className="header-background">
+        <div className="tree-logo-container">
+          <img src={treeLogo} alt="Tree Logo" className="tree-logo" />
+          <div className="falling-leaf leaf1"><img src={leaf1} alt="Leaf 1" /></div>
+          <div className="falling-leaf leaf2"><img src={leaf2} alt="Leaf 2" /></div>
+          <div className="falling-leaf leaf3"><img src={leaf3} alt="Leaf 3" /></div>
+        </div>
+        <br />
+        <br />
+        <nav className="main-nav">
+          <ul className="nav-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
       </div>
-      <br />
-      <br />
-      {/* 🔽 Navigation Bar */}
-      <nav className="main-nav">
-        <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
     </header>
   );
 };

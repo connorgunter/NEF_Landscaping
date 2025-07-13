1693e039fa4fd40bdc288d0f036f78f007b3306c
Connor Gunter <bigc5674@gmail.com>
2025-07-13 11:11:21 -0400

    updated footer url

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,17 +1,20 @@
-import './footer.css'
-
+import "./footer.css";
 
 const Footer = () => {
-return (
-<footer className="site-footer">
-  <p>
-    © 2025 New England’s Finest Landscaping. Site by{" "}
-    <a href="https://connorgunter.netlify.app/" target="_blank" rel="noopener noreferrer">
-      Connor Gunter
-    </a>.
-  </p>
-</footer>
-
-)
-}
+  return (
+    <footer className="site-footer">
+      <p>
+        © 2025 New England’s Finest Landscaping. Site by{" "}
+        <a
+          href="https://connorgunter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Connor Gunter
+        </a>
+        .
+      </p>
+    </footer>
+  );
+};
 export default Footer;
